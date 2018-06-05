9ee3ebf01b153b3c66aaa09dc4d4533b535e1a58
Dayan Perera <dayannp@gmail.com>
2018-06-05 14:07:38 -0700

    create User model for mongo db and add body parser middle ware

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/User");
+
+//@route Get api/users/test
+//@desc Test users route
+//@access Public
 
 router.get("/test", (req, res) => {
   res.json({ msg: "Your Users Route" });
 });
 
+//@route Get api/users/register
+//@desc Register a new user
+//@access Public
+router.get("/register", (req, res) => {
+  User.findOne({ email: req.body.email });
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const { postRoutes, profileRoutes, userRoutes } = require("./routes");
 const { mongoURI } = require("./config/keys");
@@ -12,6 +13,9 @@ mongoose
   .then(() => console.log("MongoDB connection successfull !!!"))
   .catch(err => console.log("MongoDB connection error -->", err));
 
+//add body parser middle ware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //use routes
 app.use("/api/users", userRoutes);
 app.use("/api/profile", profileRoutes);
