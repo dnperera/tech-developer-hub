4e4611af8907faf22db82df2ae710a344c3f5895
Dayan Perera <dayannp@gmail.com>
2018-06-17 21:46:30 -0700

    implement react router

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Register User ....</h4>
+      </div>
+    );
+  }
+}
+export default Register;
