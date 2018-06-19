90523c8cbe02a75887c5c2e076b86e4b1ffbb89e
Dayan Perera <dayannp@gmail.com>
2018-06-18 23:05:15 -0700

    create user register action and reducer in redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 import classNames from "classnames";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
 class Register extends Component {
   constructor() {
     super();
@@ -28,12 +30,13 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     };
-    axios
-      .post("/api/users/register", newUser)
-      .then(res => {
-        console.log(res.data);
-      })
-      .catch(err => this.setState({ errors: err.response.data }));
+    this.props.registerUser(newUser);
+    // axios
+    //   .post("/api/users/register", newUser)
+    //   .then(res => {
+    //     console.log(res.data);
+    //   })
+    //   .catch(err => this.setState({ errors: err.response.data }));
   };
   render() {
     const { errors } = this.state;
@@ -120,4 +123,7 @@ class Register extends Component {
     );
   }
 }
-export default Register;
+export default connect(
+  null,
+  { registerUser }
+)(Register);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,9 +1,15 @@
+import { TEST_DISPATCH } from "../actions/types";
 const initialState = {
   isAuthenticate: false,
   user: {}
 };
 export default (state = initialState, action) => {
   switch (action.type) {
+    case TEST_DISPATCH:
+      return {
+        ...state,
+        user: action.payload
+      };
     default:
       return state;
   }
