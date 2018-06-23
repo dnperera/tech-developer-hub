b4aa08e4e0eab5e3e3c9249397c7d414c5320b8a
Dayan Perera <dayannp@gmail.com>
2018-06-22 23:23:00 -0700

    display content on the edit profile

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
-
+import EditProfile from "./components/edit-profile/EditProfile";
 import "./App.css";
 import store from "./store";
 import PrivateRoute from "./components/common/PrivateRoute";
@@ -55,6 +55,13 @@ class App extends Component {
                   exact
                   path="/create-profile"
                   component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
                 />
               </Switch>
             </div>
