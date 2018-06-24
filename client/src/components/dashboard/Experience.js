b400b7064ccb67e4d8dce489499c93096d8dc744
Dayan Perera <dayannp@gmail.com>
2018-06-24 08:09:59 -0700

    complete display experience on dashboard

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import Moment from "react-moment";
+import PropTypes from "prop-types";
+
+class Experience extends Component {
+  render() {
+    const experience = this.props.experience.map(exp => (
+      <tr key={exp._id}>
+        <td>{exp.company}</td>
+        <td>{exp.title}</td>
+        <td>
+          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -{" "}
+          {exp.to === null ? (
+            "Now"
+          ) : (
+            <Moment format="YYYY/MM/DD">{exp.to}</Moment>
+          )}
+        </td>
+        <td>
+          <button className="btn btn-danger">Delete</button>
+        </td>
+      </tr>
+    ));
+
+    return (
+      <div>
+        <h4 className="mb-4">Experience Credentials</h4>
+        <table className="table table-font">
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Title</th>
+              <th>Years</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>{experience}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+Experience.propType = {};
+export default connect(null)(withRouter(Experience));
--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { getCurrentProfile, deleteAccount } from "../../actions/profileActions";
 import Spinner from "../common/Spinner";
 import ProfileActions from "./ProfileActions";
+import Experience from "./Experience";
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -30,6 +31,7 @@ class Dashboard extends Component {
             </p>
             <ProfileActions />
             {/* {TO DO exp and edu} */}
+            <Experience experience={profile.experience} />
             <div style={{ marginBottom: "60px" }}>
               <button onClick={this.onDeleteClick} className="btn btn-danger">
                 Delete My Account
